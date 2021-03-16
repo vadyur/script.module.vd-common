d18f4fa82f13b0667560bcf989a092a582e1c1de
vd <vadyur@csodessa.com>
2021-03-16 19:09:59 +0200

    +++

---
+++ lib/vdlib/util/__init__.py
@@ -0,0 +1,13 @@
+import sys
+if sys.version_info >= (3, 0):
+    from urllib.parse import urljoin, urlparse
+    from urllib.request import pathname2url
+    from urllib.request import urlopen
+else:
+    from urlparse import urljoin, urlparse
+    from urllib import pathname2url
+    from urllib2 import urlopen
+
+def path2url(path):
+    return urljoin('file:', pathname2url(path))
+
---
+++ lib/vdlib/util/string.py
@@ -0,0 +1,5 @@
+import sys
+if sys.version_info >= (3, 0):
+    uni_type = str
+else:
+    uni_type = unicode
